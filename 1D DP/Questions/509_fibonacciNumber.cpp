1bbb15869d1c41e7b907dccc8c1365fd2f33065e
mavrix21 <150045973+AdityaRane21@users.noreply.github.com>
2026-02-16 14:16:08 +0530

    Refactor Fibonacci solution with multiple approaches

--- 1D DP/Questions/509_fibonacciNumber.cpp
+++ 1D DP/Questions/509_fibonacciNumber.cpp
@@ -1,3 +1,13 @@
+// basic recursion // 2^n
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+};
+
+// memoization
 class Solution {
 public:
     int solve(int n,vector<int>&dp){
@@ -16,4 +26,48 @@ public:
 
         return solve(n,dp);
     }
+
+};
+
+// bottom up
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n <= 1)
+            return n;
+        
+        int t[n+1];
+        //State definition
+        //t[i] = ith Fibonacci Number
+        
+        t[0] = 0;
+        t[1] = 1;
+        
+        for(int i = 2; i<=n; i++) {
+            t[i] = t[i-1] + t[i-2];
+        }
+        
+        return t[n];
+        
+    }
+};
+
+// constant space 
+class Solution {
+public:
+    int fib(int n) {
+        if( n<=1) return n;
+        
+        int c;
+        int a = 0, b = 1;
+        
+        for(int i = 1; i<n; i++) {
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+    
 };
