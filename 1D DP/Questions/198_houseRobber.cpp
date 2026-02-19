4cda4ec14a9ba2370d770f226f8fca344e216fa3
mavrix21 <150045973+AdityaRane21@users.noreply.github.com>
2026-02-19 19:17:09 +0530

    Add space-optimized solution for house robber problem
    Implemented a space-optimized solution for the house robber problem using constant space.

--- 1D DP/Questions/198_houseRobber.cpp
+++ 1D DP/Questions/198_houseRobber.cpp
@@ -86,3 +86,26 @@ public:
     }
 };
 
+//Approach-3 (Space Optimized Bottom Up)
+// only we needed prev 2 values thus we used constant space instead of array 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        
+        int prevPrev = 0;
+        int prev = nums[0];
+        
+        for(int i = 2; i<=n; i++) {
+            int skip = prev;
+            int take = nums[i-1] + prevPrev;
+                
+            int temp = max(skip, take);
+            
+            prevPrev = prev;
+            prev = temp;
+        }
+        
+        return prev;
+    }
+};
